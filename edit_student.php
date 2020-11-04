173e0da0727cd0849bd58e5ce72212cf47cf3347
mrshaikat <mrshaikat0.cse@gmail.com>
2020-11-04 12:09:16 +0600

    single student and edit student design done

---
+++ edit_student.php
@@ -0,0 +1,81 @@
+<?php require_once ("templates/header.php"); ?>
+
+                                    <div class="container-fluid page__container">
+							            <div class="row" style="min-height:500px;">
+							                <div class="col-lg-12">
+                                            <h1 class="m-5"></h1>
+								
+                                                <div class="card">
+                                                    <div class="card-header">
+                                                   <h2 class=" card-title"> Edit Student</h2>
+                                                    </div>
+                                                <div class="card-body">
+                                           
+
+                                            <table class="table table-striped">
+                                                <tr>
+                                                <td>Student ID</td>
+                                                <td> <input type="text"> </td>
+                                                </tr>
+
+                                                <tr>
+                                                <td>Student Status</td>
+                                                <td><input type="text"></td>
+                                                </tr>
+
+
+                                                <tr>
+
+                                                <td>Student Name</td>
+                                                <td><input type="text"></td>
+                                                </tr>
+
+                                                <tr>
+
+                                                <td>Student E-mail</td>
+                                                <td><input type="text"></td>
+                                                </tr>
+
+                                                <tr>
+                                                <td>Student Cell</td>
+                                                <td><input type="text"></td>
+                                                </tr>
+
+                                                <tr>
+                                                <td>Student Location</td>
+                                                <td><input type="text"></td>
+                                                </tr>
+
+                                                <tr>
+                                                <td>Student Photo</td>
+                                                <td>
+                                                <img class=" img-thumbnail" style=" width:100px; height:100px;" src="student_photos/1a5d6364e4592ac1f40bdd7eb135d9e4.jpg" alt="">
+                                                </td>
+                                                </tr>
+
+                                                <tr>
+                                                <td>Upload Picture</td>
+                                                <td><input type="file"></td>
+                                                </tr>
+
+                                                <tr>
+
+                                                <td></td>
+                                                
+                                                <td><input class="mt-3 btn btn-warning" type="submit" value="Update"></td>
+                                                </tr>
+
+
+                                            </table>
+
+
+
+                                            </div>
+                                                </div>
+                            
+                                                
+                                            </div>
+							            </div>
+                                    </div>
+                                    
+<?php require_once ("templates/footer.php"); ?>
---
+++ single_student.php
@@ -0,0 +1,61 @@
+<?php require_once ("templates/header.php"); ?>
+
+                                    <div class="container-fluid page__container">
+							            <div class="row" style="min-height:500px;">
+							                <div class="col-lg-12">
+                                            <h1 class="m-5"></h1>
+								
+                                                <div class="card">
+                                                    <div class="card-header">
+                                                   <h2 class=" card-title"> Single Student</h2>
+                                                    </div>
+                                                <div class="card-body">
+                                            <img style=" width:200px; height:200px; border-radius:50%; display:block; margin: 50px auto 10px;" src="student_photos/1a5d6364e4592ac1f40bdd7eb135d9e4.jpg" alt="">
+
+                                            <table class="table table-striped table-hover">
+                                                <tr>
+                                                <td>Student ID</td>
+                                                <td>Ashikujjaman Shaikat</td>
+                                                </tr>
+
+                                                <tr>
+                                                <td>Student Status</td>
+                                                <td>Active</td>
+                                                </tr>
+
+
+                                                <tr>
+
+                                                <td>Student Name</td>
+                                                <td>Ashikujjaman Shaikat</td>
+                                                </tr>
+
+                                                <tr>
+
+                                                <td>Student E-mail</td>
+                                                <td>Ashikujjaman Shaikat</td>
+                                                </tr>
+
+                                                <tr>
+                                                <td>Student Cell</td>
+                                                <td>Ashikujjaman Shaikat</td>
+                                                </tr>
+
+                                                <tr>
+                                                <td>Student Location</td>
+                                                <td>Ashikujjaman Shaikat</td>
+                                                </tr>
+
+                                            </table>
+
+
+
+                                            </div>
+                                                </div>
+                            
+                                                
+                                            </div>
+							            </div>
+                                    </div>
+                                    
+<?php require_once ("templates/footer.php"); ?>
